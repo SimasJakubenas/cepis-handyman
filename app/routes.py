230ffe563b8ae46e9c0bdf5d2e04bf88fc96fc08
Simas Jakubenas <sjakubenas@gmail.com>
2025-10-31 00:04:17 +0200

    Add supabase db connection

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, jsonify
 import json, os
+from app.supabase_client import supabase
 
 main = Blueprint("main", __name__)
 
@@ -13,3 +14,14 @@ def gallery():
     with open(path) as f:
         images = json.load(f)
     return render_template("gallery.html", images=images)
+
+@main.route("/dbcheck")
+def dbcheck():
+    try:
+        response = supabase.table('Project').select('*').limit(1).execute()
+        if response.data is not None:
+            return f"Connected to Supabase! {response.data}"
+        else:
+            return f"Connection failed: {response}"
+    except Exception as e:
+        return f"Error connecting to Supabase: {e}"
